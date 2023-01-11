6f335aca80f28683f185aa96728a3e090d2c3e4d
Augusto <augustobdev@outlook.com>
2023-01-11 14:02:35 -0300

    feat: connect to mongodb before app listen

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,8 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL,
   port: process.env.PORT || 5050
 }
