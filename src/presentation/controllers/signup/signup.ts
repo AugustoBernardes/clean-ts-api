851e7c97febd264765c135b60488f4e8d84cfa8b
Augusto <augustobdev@outlook.com>
2023-02-02 17:26:25 -0300

    feat: ensure signupController returns 400 if validation fails

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -11,7 +11,10 @@ export class SignUpController implements Controller {
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      this.validation.validate(httpRequest.body)
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
       for (const field of requiredFields) {
