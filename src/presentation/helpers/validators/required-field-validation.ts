3d82945387f5f1ce39afda1b28cd13509b00851f
Augusto <augustobdev@outlook.com>
2023-02-05 22:36:38 -0300

    fix: error return when invalid param is received

--- src/presentation/helpers/validators/required-field-validation.ts
+++ src/presentation/helpers/validators/required-field-validation.ts
@@ -5,6 +5,7 @@ export class RequiredFieldValidation implements IValidation {
   constructor (private readonly fieldName: string) {}
   validate (input: any): Error | null {
     if (!input[this.fieldName]) {
+      console.log('MissingParamError: ', this.fieldName)
       return new MissingParamError(this.fieldName)
     }
 
