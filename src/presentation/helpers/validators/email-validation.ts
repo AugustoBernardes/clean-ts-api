5f4926e878288a21979383cc3fdbb108c0d0eec4
Augusto <augustobdev@outlook.com>
2023-02-07 19:38:33 -0300

    refactor: move Validation to protocols folder

--- src/presentation/helpers/validators/email-validation.ts
+++ src/presentation/helpers/validators/email-validation.ts
@@ -1,6 +1,6 @@
 import { InvalidParamError } from '../../errors'
 import { IEmailValidator } from '../../protocols'
-import { IValidation } from './validation'
+import { IValidation } from '../../protocols/validation'
 
 export class EmailValidation implements IValidation {
   constructor (private readonly fieldName: string, private readonly emailValidator: IEmailValidator) {}
