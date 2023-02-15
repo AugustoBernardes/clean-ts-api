523f491c7c47b71098800cdd2600512b435fa523
Augusto <augustobdev@outlook.com>
2023-02-14 22:27:34 -0300

    feat: ensure JwtAdapter return a token when ign succeeds

--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken'
 export class JwtAdapter implements IEncrypter {
   constructor (private readonly secret: string) {}
   async encrypt (value: string): Promise<string> {
-    await jwt.sign({ id: value }, this.secret)
-    return await new Promise(resolve => resolve('null'))
+    const accessToken = await jwt.sign({ id: value }, this.secret)
+    return accessToken
   };
 }
