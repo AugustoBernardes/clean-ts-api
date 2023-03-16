7f14193a663058462b3e5540ad01750967a4755f
AugustoBernardes <augustobdev@outlook.com>
2023-03-15 22:26:47 -0300

    refactor: allows api to run locally or with docker

--- src/main/factories/controllers/login/signup/signup-controller-factory.ts
+++ src/main/factories/controllers/login/signup/signup-controller-factory.ts
@@ -1,11 +1,11 @@
 import { SignUpController } from '@/presentation/controllers/login/signup/signup-controller'
 import { Controller } from '@/presentation/protocols'
-import { makeLoginValidation } from '../login/login-validation-factory'
 import { makeDbAuthentication } from '@/main/factories/usecases/account/authentication/db-authentication-factory'
 import { makeDbAddAccount } from '@/main/factories/usecases/account/add-account/db-add-account-factory'
 import { makeLogControllerDecorator } from '@/main/factories/decorators/log-controller-decorator-factory'
+import { makeSignUpValidation } from './signup-validation-factory'
 
 export const makeSignUpController = (): Controller => {
-  const controller = new SignUpController(makeDbAddAccount(), makeLoginValidation(), makeDbAuthentication())
+  const controller = new SignUpController(makeDbAddAccount(), makeSignUpValidation(), makeDbAuthentication())
   return makeLogControllerDecorator(controller)
 }
