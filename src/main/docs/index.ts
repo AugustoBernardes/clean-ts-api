c1d29ebf185ed6fe503c99b73c70468d324c8711
AugustoBernardes <augustobdev@outlook.com>
2023-05-03 07:32:45 -0300

    feat: add swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,8 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Clean Node Api',
+    description: 'Api from course to realize surveys between developers',
+    version: '1.0.0'
+  }
+}
