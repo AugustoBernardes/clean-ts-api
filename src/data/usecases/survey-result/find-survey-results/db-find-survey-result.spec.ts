db605c0a6e13f9d2ff9553e30c3907216de59664
Augusto Bernardes <augustobdev@outlook.com>
2023-09-26 20:40:46 -0300

    refactor: move sut creation to a factory

--- src/data/usecases/survey-result/find-survey-results/db-find-survey-result.spec.ts
+++ src/data/usecases/survey-result/find-survey-results/db-find-survey-result.spec.ts
@@ -3,17 +3,34 @@ import { mockResultSurveyById } from '@/domain/test'
 import { DbFindSurveyResult } from './db-find-survey-result'
 import { IFindSurveyResultRepository, IFindSurveyResultParams } from './db-find-survey-result-protocols'
 
-describe('DbLoadSurveyResult useCase', () => {
+const mockFindSurveyResultRepositoryStub = (): IFindSurveyResultRepository => {
   class FindSurveyResultRepositoryStub implements IFindSurveyResultRepository {
     async findSurveyResult (data: IFindSurveyResultParams): Promise<IFindSurveyResultModel[]> {
       return await Promise.resolve(mockResultSurveyById())
     }
   }
 
+  return new FindSurveyResultRepositoryStub()
+}
+
+type SutTypes = {
+  sut: DbFindSurveyResult
+  findSurveyResultRepositoryStub: IFindSurveyResultRepository
+}
+
+const makeSut = (): SutTypes => {
+  const findSurveyResultRepositoryStub = mockFindSurveyResultRepositoryStub()
+  const sut = new DbFindSurveyResult(findSurveyResultRepositoryStub)
+  return {
+    sut,
+    findSurveyResultRepositoryStub
+  }
+}
+
+describe('DbLoadSurveyResult useCase', () => {
   test('should call FindSurveyResultRepository with correct values', async () => {
-    const findSurveyResultRepositoryStub = new FindSurveyResultRepositoryStub()
+    const { sut, findSurveyResultRepositoryStub } = makeSut()
     const loadByIdSpy = jest.spyOn(findSurveyResultRepositoryStub, 'findSurveyResult')
-    const sut = new DbFindSurveyResult(findSurveyResultRepositoryStub)
     await sut.findSurveyResult({ surveyId: 'any_survey_id' })
     expect(loadByIdSpy).toHaveBeenCalledWith({ surveyId: 'any_survey_id' })
   })
